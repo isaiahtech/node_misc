34b840b5780b2f270e96949def6437f3ed88a6dc
isaiahtech <i.dasen@f5.com>
2018-08-22 22:55:39 -0700

    local system interaction

---
+++ 1-proc-streams.js
@@ -0,0 +1,18 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(chunk) {
+    process.stdout.write('Data! -> ' + chunk);
+});
+
+process.stdin.on('end', function () {
+    process.stderr.write('End!\n');
+});
+
+// Adding on proc-events
+
+process.on('SIGTERM', function() {
+    process.stderr.write("Why are you trying to kill me?!  >:/");
+});
+
+console.log("Node is running as process #" + process.pid);
