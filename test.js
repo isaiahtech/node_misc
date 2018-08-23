e6c91706b28061a4504ad901f80fbf092e64d6e4
Isaiah Dasen <dothackdasen@gmail.com>
2018-08-23 16:12:09 -0700

    syncing test files

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+
+To do...
+Prompt for Password
+Auth to iHealth API:
+    https://ihealth-api.f5.com/qkview-analyzer/api/qkviews/10054275/commands/68e90d0ffb8560680f3712261e57bf0e2f3d94c8
+Return command output, store as variable
+
+*/
