d56d18ffb161363d7e6e7e4623d05516a770a273
isaiahtech <i.dasen@f5.com>
2018-08-25 22:14:02 -0700

    Adding assertions/testing

---
+++ mathfun.js
@@ -0,0 +1,18 @@
+var maxTime = 1000;
+
+var evenDoubler = function(v, callback) {
+    //code
+};
+
+var evenDoublerSync = function(v) {
+    if (v%2) {
+        throw(new Error("Odd input"));
+    } else {
+        return(v*2);
+    }
+};
+
+module.exports.evenDoubler = evenDoubler;
+module.exports.evenDoublerSync = evenDoublerSync;
+
+module.exports.foo = "bar";
